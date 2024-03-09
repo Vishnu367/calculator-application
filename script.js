6f17a5c211b53dcbd575b1920abfc31073cd390c
Vishnu Vardhan <vishnuvardhanachari367@gmail.com>
2024-03-09 20:43:01 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const numberButton = document.querySelectorAll("[data-Number]")
+const operationButton = document.querySelectorAll("[data-operation]")
+const equalsButton = document.querySelector("[data-equals]")
+const clearAllButton = document.querySelector("[data-clear-all]")
+const deleteButton = document.querySelector("[data-delete]")
+const prevOperationTextElement = document.querySelector("[data-prev-text-container]")
+const currentOperationTextElement = document.querySelector("[data-current-text-container]")
+
+
+
+console.log(operationButton)
